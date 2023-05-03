a804c64da98768bf9ae296e7375bf4ad30a31192
sabrizlt <sabrizaltini6@gmail.com>
2023-05-03 09:32:48 +0200

    first commit

--- src/it/epicode/be/Helloworld.java
+++ src/it/epicode/be/Helloworld.java
@@ -27,7 +27,6 @@ public class Helloworld {
     System.out.println(stringaTre + ' ' + stringaDue + ' ' + stringaUno);
   }
 
-  //Esercizio #2
   public static void moltiplica(int n1, int n2) {
     System.out.println(n1 * n2);
   }
